4cf098dab80fb3bc241c80fb0c01581ba261daa8
acp441 <45313740+acp441@users.noreply.github.com>
2019-05-02 16:27:13 +0200

    Add files via upload

---
+++ Tonausgabe_O_Tannenbaum_02052019/Coldplay_Clocks.ino
@@ -0,0 +1,42 @@
+
+
+void clocks() {
+  es3(takt/8);
+  ais3(takt/8);
+  g3(takt/8);
+  es3(takt/8);
+  ais3(takt/8);
+  g3(takt/8);
+  es3(takt/8);
+  ais3(takt/8);
+
+  cis3(takt/8);
+  ais3(takt/8);
+  set1(resetT);
+  f3(takt/8);
+  cis3(takt/8);
+  ais3(takt/8);
+  f3(takt/8);
+  cis3(takt/8);
+  ais3(takt/8);
+
+  cis3(takt/8);
+  ais3(takt/8);
+  set1(resetT);
+  f3(takt/8);
+  cis3(takt/8);
+  ais3(takt/8);
+  f3(takt/8);
+  cis3(takt/8);
+  ais3(takt/8);
+
+  c3(takt/8);
+  as3(takt/8);
+  f3(takt/8);
+  c3(takt/8);
+  as3(takt/8);
+  f3(takt/8);
+  c3(takt/8);
+  as3(takt/8);
+
+}
---
+++ Tonausgabe_O_Tannenbaum_02052019/Tonmatrix_testen.ino
@@ -0,0 +1,13 @@
+void testen(){
+
+ton(0,6,(takt/4));
+ton(1,6,(takt/4));
+ton(2,6,(takt/4));
+ton(3,6,(takt/4));
+ton(4,6,(takt/4)); 
+ton(5,6,(takt/4));
+ton(7,6,(takt/4));
+ton(6,6,(takt/4));
+ton(0,0, (takt/4));
+
+}
