467fe5eb08a1685b96859e9e14b49399aeacb6d2
acp441 <45313740+acp441@users.noreply.github.com>
2019-05-02 13:36:18 +0200

    Add files via upload
    Beispiel für die Ausgabe von MIDI-Noten

---
+++ MIDI_Test.ino
@@ -0,0 +1,18 @@
+#include <MIDI.h>
+ // Created and binds the MIDI interface to the default hardware Serial port
+ MIDI_CREATE_DEFAULT_INSTANCE();
+
+ void setup()
+ {
+     MIDI.begin(MIDI_CHANNEL_OMNI);  // Listen to all incoming messages
+ }
+
+ void loop()
+ {
+     MIDI.read();
+     delay(500);
+     MIDI.sendNoteOn(30, 127, 1);
+     delay(500);
+     MIDI.sendNoteOff(30, 127, 1);
+     delay(500);
+ }
